a875de78e6c28a605b7826d833bdca49bd0c4812
MinboShim <80995486+tlaalsqh7414@users.noreply.github.com>
2021-11-17 18:25:52 +0900

    Add detect API url

--- FD/account/urls.py
+++ FD/account/urls.py
@@ -1,17 +1,12 @@
 from django.urls import path
 from django.conf.urls import include, url
 from . import views
-from rest_framework_simplejwt.views import(
-    TokenObtainPairView,
-    TokenRefreshView,
-)
+
+
 urlpatterns = [
     url('regist_user', views.RegistUser.as_view(), name='regist_user'),
     url('app_login', views.AppLogin.as_view(), name='app_login'),
     url('profile_meal', views.ProfileMeal.as_view(), name='profile_meal'),
     url('community_img', views.CommunityImg.as_view(), name='community_img'),
     url('detect', views.Detect.as_view(), name='detect')
-  #  path('token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-  #  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
 ]
