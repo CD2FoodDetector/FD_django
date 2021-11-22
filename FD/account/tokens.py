8be50823a84362981cf390bdd20deba459ac3b51
SeYeon Kim <sydsh19@naver.com>
2021-11-22 13:17:50 +0900

    add refresh token

--- FD/account/tokens.py
+++ FD/account/tokens.py
@@ -8,6 +8,21 @@ def create_token(id):
     encoded = jwt.encode({'exp': datetime.datetime.utcnow() + datetime.timedelta(days=1), 'id' : id}, SECRET_PRE, algorithm='HS256')
     return encoded
 
+def create_refresh(id):
+    encoded = jwt.encode({'exp': datetime.datetime.utcnow() + datetime.timedelta(days=7), 'id': id}, SECRET_PRE,
+                         algorithm='HS256')
+    return encoded
+
+def check_refresh(token, id):
+    try:
+        jwt.decode(token, SECRET_PRE, algorithms='HS256')
+    except jwt.ExpiredSignatureError:
+        return "expiredSignature"
+    except jwt.InvalidTokenError:
+        return "invalid"
+    else:
+        return create_token(id)
+
 def validate_token(token):
     try:
         jwt.decode(token, SECRET_PRE, algorithms='HS256')
